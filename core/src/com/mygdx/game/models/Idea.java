ca3c177500448c229fb823b45301018a421c2949
VildeStabell <Vilde.voss.stabell@gmail.com>
2021-03-15 15:08:25 +0100

    #2 - Fixed documentation and added some error handling in the constructor.

--- core/src/com/mygdx/game/models/Idea.java
+++ core/src/com/mygdx/game/models/Idea.java
@@ -1,16 +1,20 @@
 package com.mygdx.game.models;
 
-/** The Idea model stores one idea from a player
- * @param idea: the string containing the idea.
- * @param player: the player that came up with the idea
+/**
+ * The Idea model stores one idea from a player
+ * idea: the string containing the idea.
+ * player: the player that came up with the idea
  * This class implements the MVC pattern.
- * **/
+ */
 
 public class Idea {
     private final String idea;
     private final Player player;
 
     public Idea(String idea, Player player) {
+        if(idea == null || idea.equals("")|| player == null)
+            throw new IllegalArgumentException("Both an idea and a player must be provided");
+
         this.idea = idea;
         this.player = player;
     }

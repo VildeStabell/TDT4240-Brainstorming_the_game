5e5f72f139f62e11c43c2cb6c5be09aa1ef880b1
VildeStabell <Vilde.voss.stabell@gmail.com>
2021-03-15 14:29:06 +0100

    #2 - Implemented Brain model

---
+++ core/src/com/mygdx/game/models/Brain.java
@@ -0,0 +1,34 @@
+package com.mygdx.game.models;
+
+import java.util.ArrayList;
+
+/** The Brain model stores related ideas
+ * @param ideas: the ArrayList ideas contains
+ * */
+
+public class Brain {
+    private ArrayList<Idea> ideas = new ArrayList<>();
+
+    public ArrayList<Idea> getIdeas() {
+        return ideas;
+    }
+
+    public Idea getIdea(int n) {
+        if(n < 0 || n >= getSize())
+            throw new IndexOutOfBoundsException("Index n has to be between 0 and "
+                    + getSize() + "but was " + n);
+
+        return ideas.get(n);
+    }
+
+    public int getSize() {
+        return ideas.size();
+    }
+
+    public void addIdea(Idea idea) {
+        if(idea == null)
+            throw new IllegalArgumentException("Idea cannot be null");
+
+        ideas.add(idea);
+    }
+}

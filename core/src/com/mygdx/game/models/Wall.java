37cd5ab9f8dce2eeb28ef7cfefea6055ad299a3e
VildeStabell <Vilde.voss.stabell@gmail.com>
2021-03-15 15:25:28 +0100

    #3 - Implemented the Wall model

---
+++ core/src/com/mygdx/game/models/Wall.java
@@ -0,0 +1,42 @@
+package com.mygdx.game.models;
+
+/**
+ * The Wall model represents a wall, and stores how damaged it currently is.
+ * maxHitpoints: The amount of hp the wall starts out with.
+ * hitPoints: The amount of hp the wall currently has.
+ * This class implements the MVC pattern.
+ */
+
+public class Wall {
+    private final int maxHitPoints;
+    private int hitPoints;
+
+    public Wall(int hitPoints) {
+        if(hitPoints <= 0)
+            throw new IllegalArgumentException("Hitpoints has to be initialized to a value above 0");
+
+        this.maxHitPoints = hitPoints;
+        this.hitPoints = hitPoints;
+    }
+
+    public int getMaxHitPoints() {
+        return maxHitPoints;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    // Returns true if the wall is broken by the damage, otherwise returns false
+    public boolean takeDmg(int dmg) {
+        if(hitPoints <= 0)
+            throw new IllegalArgumentException("Damage taken has to be above 0");
+
+        if(dmg >= hitPoints) {
+            hitPoints = 0;
+            return true;
+        }
+        hitPoints -= dmg;
+        return false;
+    }
+}

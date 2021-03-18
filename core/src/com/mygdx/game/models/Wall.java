cf5f9b8426231f6c5c5f8e935fe091c86b7855ec
VildeStabell <Vilde.voss.stabell@gmail.com>
2021-03-18 13:38:11 +0100

    #25 - Created WallTest and added some error handling to Wall.takeDmg()

--- core/src/com/mygdx/game/models/Wall.java
+++ core/src/com/mygdx/game/models/Wall.java
@@ -33,8 +33,10 @@ public class Wall {
      * @return true if the wall is broken by the damage, otherwise returns false
       */
     public boolean takeDmg(int dmg) {
-        if(hitPoints <= 0)
+        if(dmg <= 0)
             throw new IllegalArgumentException("Damage taken has to be above 0");
+        if(hitPoints <= 0)
+            throw new IllegalStateException("Cannot take damage if wall is already destroyed");
 
         if(dmg >= hitPoints) {
             hitPoints = 0;

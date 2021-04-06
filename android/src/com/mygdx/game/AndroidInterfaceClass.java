5d3b8d01a537c8ee60913ff56a98970a96b56561
maisergodt <maihtran.99@gmail.com>
2021-04-06 16:17:30 +0200

    Javadoc for AndroidInterfaceClass

--- android/src/com/mygdx/game/AndroidInterfaceClass.java
+++ android/src/com/mygdx/game/AndroidInterfaceClass.java
@@ -11,17 +11,32 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import static android.content.ContentValues.TAG;
 
-
+/**
+ * A class to access the Firebase package through an interface
+ * The database is a NoSQL database which the data is stored as a key-value pair
+ *
+ * database: the realtime database in Firebase
+ * myRef: Access location in database for read/write
+ *
+ * Need to implement relevant methods according to the Brainstorming game
+ */
 public class AndroidInterfaceClass implements FirebaseInterface {
 
     private final FirebaseDatabase database;
     private DatabaseReference myRef;
 
+    /**
+     * Initialize the database by passing an URL to the database
+     * Pointing to a "key" addressed by the path to store and write data whenever myRef is used.
+     */
     public AndroidInterfaceClass(){
         database = FirebaseDatabase.getInstance("https://brainstorming-1fa06-default-rtdb.europe-west1.firebasedatabase.app/");
         myRef = database.getReference("message");
     }
 
+    /**
+     * Output the value every time myRef is updated
+     */
     @Override
     public void setOnValueChangedListener() {
         myRef.addValueEventListener(new ValueEventListener() {

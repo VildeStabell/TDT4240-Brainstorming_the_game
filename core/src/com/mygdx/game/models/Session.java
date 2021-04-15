d5c1224a48aeab2132f86d1be79070e99aacc8c9
VildeStabell <Vilde.voss.stabell@gmail.com>
2021-04-15 15:27:14 +0200

    #9 - Implemented the necessary methods in MultiplayerSession and fixed typo in Round

--- core/src/com/mygdx/game/models/Session.java
+++ core/src/com/mygdx/game/models/Session.java
@@ -5,28 +5,50 @@ import java.util.ArrayList;
 /**
  * The abstract Session model defines methods and variables that is needed regardless of
  * whether the session is singleplayer or multiplayer.
+ *
+ * This class implements the MVC pattern.
+ * The parameters given in the constructor makes it easier to implement the quality requirement M1.
  */
 
 public abstract class Session {
-    protected ArrayList<Brain> brains;
+    protected ArrayList<Brain> selectedBrains;
+    protected ArrayList<Brain> allBrains;
     protected ArrayList<Round> rounds;
-    protected int maxHitPoints;
-    protected int brainDamage;
-    protected int maxSelectedBrains;
+    protected final int maxHitPoints;
+    protected final int brainDamage;
+    protected final int maxSelectedBrains;
+    protected final int maxRounds;
+    protected boolean activeRound;
 
-    public Session(int maxHitPoints, int brainDamage, int maxSelectedBrains) {
-        this.brains = new ArrayList<>();
+    public Session(int maxHitPoints, int brainDamage, int maxSelectedBrains, int maxRounds) {
+        this.selectedBrains = new ArrayList<>();
+        this.allBrains = new ArrayList<>();
         this.rounds = new ArrayList<>();
         this.maxHitPoints = maxHitPoints;
         this.brainDamage = brainDamage;
         this.maxSelectedBrains = maxSelectedBrains;
+        this.maxRounds = maxRounds;
+        this.activeRound = false;
     }
 
-    public ArrayList<Brain> getBrains() {
-        return brains;
+    public ArrayList<Brain> getSelectedBrains() {
+        return selectedBrains;
     }
+
+    public ArrayList<Brain> getAllBrains() {
+        return allBrains;
+    }
+
     public ArrayList<Round> getRounds() {
         return rounds;
+    }
+
+    public int getNumOfRounds() {
+        return rounds.size();
+    }
+
+    public Round getCurrentRound() {
+        return rounds.get(rounds.size()-1);
     }
 
     /**

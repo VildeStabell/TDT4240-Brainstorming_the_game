785ec74a06c5a51df194784b3e450727efc18a11
maisergodt <maihtran.99@gmail.com>
2021-04-26 01:17:49 +0200

    #44 Implemented the lobby screen for waiting for the game to start. Not coupled with controller or any logic.

---
+++ core/src/com/mygdx/game/screens/LobbyScreen.java
@@ -0,0 +1,132 @@
+package com.mygdx.game.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.List;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.StringBuilder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Displaying the lobby view before starting the game.
+ * The host will receive a digit code appearing at top center of screen.
+ * Incoming players will be displayed on the white list.
+ *
+ * startGame: start the game when at least another player has joined
+ * digitCodeLabel: presents the digit code
+ * activePlayers: List to display the players that have joined the session
+ * (NOTE: this is not from the standard java.util package but LibGdx Scene2D ui)
+ * waitingPlayers: ArrayList of the players waiting to join. Currently a dummy list
+ * numberOfPlayers: Keeping track of player that are added to the waiting list
+ * Implementing MVC pattern.
+ */
+
+public class LobbyScreen extends BaseScreen {
+
+    private TextButton startGame;
+    private Label digitCodeLabel;
+    private List<String> activePlayers;
+
+
+    // TODO: DUMMY values, remove
+    ArrayList<String> waitingPlayers = new ArrayList<>(Arrays.asList("Maisergodt", "VforVigor", "sigtheidiot", "sofuslofus", "emma123"));
+    private int numberOfPlayers = waitingPlayers.size();
+
+    public LobbyScreen(GameScreenManager gsm){
+        super(gsm, "textures/backgrounds/standardBackground.png");
+    }
+
+    @Override
+    public void show(){
+        super.show();
+        digitCodeLabel = new Label("", skin);
+        activePlayers = new List<String>(skin);
+        startGame = new TextButton("START GAME", skin);
+        // centering the table layout
+        table.setBounds(
+                Gdx.graphics.getWidth()/4f,
+                Gdx.graphics.getHeight()/4f,
+                Gdx.graphics.getWidth()/2f,
+                Gdx.graphics.getHeight()/2f
+        );
+
+        startGame.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y){
+                // To simulate when a player join a session
+                waitingPlayers.add("New plaer");
+//                if(!activePlayers.isEmpty()){
+//                    // TODO: Start game
+//                }
+            }
+        });
+
+        activePlayers.setAlignment(Align.center);
+        activePlayers.setItems(getPlayerListAsString(waitingPlayers));
+        table.row();
+        table.add(digitCodeLabel);
+        table.row();
+        table.add(activePlayers);
+        table.row();
+        table.add(startGame);
+        // TODO: remove
+        // table.setDebug(true);
+    }
+
+    @Override
+    public void render(float delta){
+        super.render(delta);
+        stage.draw();
+        // TODO: check if a waiting player has been added to active list of players
+        if(waitingPlayers.size() > numberOfPlayers){
+            numberOfPlayers = waitingPlayers.size();
+            activePlayers.setItems(getPlayerListAsString(waitingPlayers));
+        }
+
+        // TODO: show only digit code for host
+//        if(isHosting){
+//            digitCodeLabel.setText(getDigitCodeLabel());
+//        }
+
+    }
+
+    @Override
+    public void pause() {}
+
+    @Override
+    public void resume() {}
+
+    @Override
+    public void hide() {
+        super.dispose();
+    }
+
+    private String getDigitCodeLabel(){
+        // TODO: digit code
+        String digitCode = "1234";
+        return String.format("Digit code: %s", digitCode);
+    }
+
+
+    /**
+     * Passing a list of players to setItems(list) will also include symbols
+     * like [],
+     * @param playerList: List of players
+     * @return concatenated player's name followed by a newline
+     */
+    private String getPlayerListAsString(ArrayList<String> playerList){
+        StringBuilder playersAsString = new StringBuilder();
+        for(String player : playerList){
+            playersAsString.append(player);
+            playersAsString.append("\n");
+        }
+        return playersAsString.toString();
+    }
+
+
+}

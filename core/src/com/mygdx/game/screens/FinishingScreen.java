4d5876f24a8143fdaefe3b2a026a76f89e246796
maisergodt <maihtran.99@gmail.com>
2021-04-27 16:14:26 +0200

    Add return button

--- core/src/com/mygdx/game/screens/FinishingScreen.java
+++ core/src/com/mygdx/game/screens/FinishingScreen.java
@@ -2,8 +2,11 @@ package com.mygdx.game.screens;
 
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.List;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
 import com.mygdx.game.Controller;
 
@@ -14,6 +17,7 @@ public class FinishingScreen extends BaseScreen {
 
     private Label totalSelectedBrains, eliminatedBrains;
     private List<String> selectedBrainsList, eliminatedBrainsList;
+    private TextButton returnButton;
 
     // TODO: controller
     public FinishingScreen(){
@@ -27,6 +31,7 @@ public class FinishingScreen extends BaseScreen {
         eliminatedBrains = new Label("Your brains", skin); //Change to your brains
         selectedBrainsList = new List<String>(skin);
         eliminatedBrainsList = new List<String>(skin);
+        returnButton = new TextButton("Return to menu", skin);
         selectedBrainsList.setAlignment(Align.center);
         eliminatedBrainsList.setAlignment(Align.center);
 
@@ -34,17 +39,31 @@ public class FinishingScreen extends BaseScreen {
         selectedBrainsList.setItems(getIdeasAsString(Controller.getInstance().getFinalBrains()));
         eliminatedBrainsList.setItems(getIdeasAsString(Controller.getInstance().getPlayersBrains()));
 
+        returnButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y){
+                // TODO: return to menuscreen
+                gsm.setScreen(GameScreenManager.ScreenEnum.MENU);
+            }
+        });
+        returnButton.setPosition(
+                Gdx.graphics.getWidth()/2f-returnButton.getWidth()/2f,
+                0
+        );
+        stage.addActor(returnButton);
         table.setBounds(
                 Gdx.graphics.getWidth()/4f,
                 Gdx.graphics.getHeight()/10f,
                 Gdx.graphics.getWidth()/2f,
-                Gdx.graphics.getHeight()/1.2f
+                Gdx.graphics.getHeight()/1.1f
         );
         table.add(totalSelectedBrains).expandX().top();
         table.add(eliminatedBrains).expandX().top();
         table.row();
         table.add(selectedBrainsList).expandX().height(Gdx.graphics.getHeight()/1.2f);
         table.add(eliminatedBrainsList).expandX().height(Gdx.graphics.getHeight()/1.2f);
+        table.row();
+        table.setDebug(true);
 
     }
 
@@ -67,7 +86,6 @@ public class FinishingScreen extends BaseScreen {
 
     @Override
     public void hide() {
-
         super.dispose();
     }
 
